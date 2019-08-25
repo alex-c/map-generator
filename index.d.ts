293b606083b31dd29ae1d54e738f3fed9ff1174a
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-25 20:33:25 +0200

    Added typings

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for mapgen 0.1.0
+// Project: mapgen
+// Definitions by: Alexandre Charoy <https://github.com/alex-c>
+
+export = MapGenerator;
+
+declare class MapGenerator {
+  generateEmptyMap(width: number, height: number): number[][];
+  step(x: number, y: number, direction: number[]): number[];
+  validatePosition(map: number[][], x: number, y: number): boolean;
+  generateMap(config?: MapGenerator.MapConfig): number[][];
+}
+
+declare namespace MapGenerator {
+  export interface MapConfig {
+    numberOfPaths?: number;
+    minPathLength?: number;
+    maxPathLength?: number;
+    maxWidth?: number;
+    maxHeight?: number;
+    startPosition?: Position;
+    seed?: string;
+  }
+  export interface Position {
+    x: number;
+    y: number;
+  }
+}
